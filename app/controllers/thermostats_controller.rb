1b3e48d9fea3a2c9f915fed4a790dfc8d512dfea
muratrahmikurtul <muratrahmikurtul@gmail.com>
2021-03-23 20:34:47 +0100

    Add Readings and Thermostats Controllers

---
+++ app/controllers/thermostats_controller.rb
@@ -0,0 +1,2 @@
+class ThermostatsController < ApplicationController
+end
---
+++ test/controllers/thermostats_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ThermostatsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
