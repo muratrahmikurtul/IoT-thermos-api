ee460a1176bed7e1331392573ef1baa093aed79e
muratrahmikurtul <muratrahmikurtul@gmail.com>
2021-03-23 20:37:09 +0100

    Add assosications and validations to models

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,12 @@
 class Reading < ApplicationRecord
+  #Associations
   belongs_to :thermostat
+
+
+  #Validations
+  validates :number, presence: true, numericality: { only_integer: true }
+  validates :temperature, presence: true, numericality: { only_float: true }
+  validates :humidity, presence: true, numericality: { only_float: true }
+  validates :battery_charge, presence: true, numericality: { only_float: true }
+
 end
--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,2 +1,8 @@
 class Thermostat < ApplicationRecord
+  #Associations
+  has_many :readings, dependent: :destroy
+
+  #Validations
+  validates :household_token, presence: true, uniqueness: true
+  validates :location, presence: true
 end
